f8d994247425dccbf4a41a94d180a4719d698f9c
yuken <yuken0906dev@gmail.com>
2022-12-29 00:41:37 +0900

    compare ajax tools

--- src/App.js
+++ src/App.js
@@ -1,5 +1,62 @@
+import { useState } from "react";
+import axios from "axios";
+
+const AjaxSample = () => {
+  const [xhrResponse, setXhrResponse] = useState();
+  const [fetchResponse, setFetchResponse] = useState();
+  const [axiosResponse, setAxiosResponse] = useState();
+
+  // ピカチュウの情報を取得
+  const url = "https://pokeapi.co/api/v2/pokemon/pikachu";
+
+  // XMLHttpRequestsを利用してリクエストを送る
+  const requestWithXHR = () => {
+    function reqListener() {
+      console.log("response from XHR", this.responseText);
+      setXhrResponse("get from XHR");
+    }
+    const req = new XMLHttpRequest();
+    req.addEventListener("load", reqListener);
+    req.open("GET", url);
+    req.send();
+  };
+
+  // fetch APIを利用してリクエストを送る
+  const requestWithFetch = () => {
+    fetch(url).then((response) => {
+      console.log("response from fetch", response);
+      setFetchResponse("get from fetch");
+    });
+  };
+
+  // axiosを利用してリクエストを送る
+  const requestWithAxios = () => {
+    axios.get(url).then((response) => {
+      console.log("response from axios", response);
+      setAxiosResponse("get from axios");
+    });
+  };
+
+  return (
+    <>
+      <>
+        <button onClick={requestWithXHR}>XHR</button>
+        <div>{xhrResponse}</div>
+      </>
+      <>
+        <button onClick={requestWithFetch}>fetch</button>
+        <div>{fetchResponse}</div>
+      </>
+      <>
+        <button onClick={requestWithAxios}>axios</button>
+        <div>{axiosResponse}</div>
+      </>
+    </>
+  );
+};
+
 function App() {
-  return <div>Hello World</div>;
+  return <AjaxSample />;
 }
 
 export default App;
